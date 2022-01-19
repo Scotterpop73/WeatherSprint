4c1c7c0f117f477c4220d0ad17a736c18989ce74
Scotterpop73 <sctzone@aol.com>
2022-01-19 15:16:24 -0800

    Worked on the html

--- scripts/app.js
+++ scripts/app.js
@@ -2,18 +2,42 @@ let weatherData = [];
 let city = document.getElementById('city');
 
 function GetWeather(){
-    fetch("http://api.openweathermap.org/data/2.5/forecast?q=Sacramento,us&units=imperial&APPID=5a80bfdde52cfa00862ac559e5485d43"
+    fetch("http://api.openweathermap.org/data/2.5/forecast?q=Green Bay,us&units=imperial&APPID=5a80bfdde52cfa00862ac559e5485d43"
     ).then(response => response.json()
     ).then(data => {
         weatherData = data;
         console.log(weatherData);
         city.textContent = "" +weatherData.city.name+ ", " +weatherData.city.country; //city name and country
-        console.log(weatherData.list[0]); //current day forecast
-        console.log(weatherData.list[1]); //next day forecast
-        console.log(weatherData.list[2]); //third day forecast
-        console.log(weatherData.list[3]); //fourth day forecast
-        console.log(weatherData.list[4]); //fifth day forecast
-        console.log(weatherData.list[5]); //sixth day forecast
+        // console.log(Math.floor(weatherData.list[0].main.temp)); //current day forecast
+        // console.log(Math.floor(weatherData.list[1].main.temp)); //next day forecast
+        // console.log(Math.floor(weatherData.list[2].main.temp)); //third day forecast
+        // console.log(Math.floor(weatherData.list[3].main.temp)); //fourth day forecast
+        // console.log(Math.floor(weatherData.list[4].main.temp)); //fifth day forecast
+        // console.log(Math.floor(weatherData.list[5].main.temp)); //sixth day forecast
+        // console.log(weatherData.city.sunrise); //sunrise
+        // console.log(weatherData.city.sunset); //sunset
+        // console.log(Math.floor(weatherData.list[0].wind.speed)); //wind speed
+        // console.log(weatherData.list[0].weather[0].description); //description of weather
+        // console.log(weatherData.list[0].main.pressure); //pressure
+        // console.log(weatherData.list[0].main.humidity); //humidity
+
+        let obj = {
+            cityName: weatherData.city.name,
+            cityCountry: weatherData.city.country,
+            currentDayTemp: Math.floor(weatherData.list[1].main.temp),
+            nextDayTemp: Math.floor(weatherData.list[1].main.temp),
+            thirdDayTemp: Math.floor(weatherData.list[2].main.temp),
+            fourthDayTemp: Math.floor(weatherData.list[3].main.temp),
+            fifthDayTemp: Math.floor(weatherData.list[4].main.temp),
+            sixthDayTemp: Math.floor(weatherData.list[5].main.temp),
+            sunrise: weatherData.city.sunrise,
+            sunset: weatherData.city.sunset,
+            wind: Math.floor(weatherData.list[0].wind.speed),
+            weatherDescription: weatherData.list[0].weather[0].description,
+            pressure: weatherData.list[0].main.pressure,
+            humidity: weatherData.list[0].main.humidity
+        };
+        console.log(obj);
     })
 }
 
